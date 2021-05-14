f2e5aae19e26334feaa229729b488871d1930fc6
Patrick Wulfe <wulfep@gmail.com>
2021-05-13 17:10:32 -0700

    switching from webview for oauth to url_launcher with uni_links using webview doesn't work on linux since it doesn't have a standard webview

--- lib/login/utils/constants.dart
+++ lib/login/utils/constants.dart
@@ -2,5 +2,5 @@ import 'dart:io';
 
 final authorizationEndpoint = 'https://myanimelist.net/v1/oauth2/authorize';
 final tokenEndpoint = 'https://myanimelist.net/v1/oauth2/token';
-final redirectUrl = 'https://localhost/oauth';
+final redirectUrl = 'https://pwulfe.animesorcerer/oauth';
 final credentialsFile = File('~/.com.wulfep.anime_sorcerer/credentials.json');
